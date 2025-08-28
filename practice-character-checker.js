a6362825381555d67285d7039db60d266bef7a83
jillian.davis96 <jilliandavis96@outlook.com>
2025-08-28 18:29:48 -0500

    Changed method of readline-sync

--- practice-character-checker.js
+++ practice-character-checker.js
@@ -1,13 +1,13 @@
-const readlineSync = require('readline-sync');
+const input = require('readline-sync');
 
 // Prompt user for a word or phrase
-const userInput = readlineSync.question('Enter a word or phrase: ');
+let userInput= input.question('Enter a word or phrase: ');
 
 // Prompt user for an index number
-const index = readlineSync.question('Enter an index number: ');
+let index = input.question('Enter an index number: ');
 
 // Use bracket notation to access the character
-const character = userInput[index];
+let character = userInput[index];
 
 // Print out the character
 console.log(character);
